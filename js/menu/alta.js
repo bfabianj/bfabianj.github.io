7eac9aa4bd91b9a84df352a9f21bb869ea2fa87b
bfabianj <fabianbarnes@gmail.com>
2022-02-02 21:19:39 -0300

    Alta y Carrito. LocalStorage y Mockapi

--- js/menu/alta.js
+++ js/menu/alta.js
@@ -1,8 +1,24 @@
-let productos = null
-
 let inputs = null
 let form = null
 let button = null
+
+const regExpValidar = [
+    /^.+$/, // regexp nombre
+    /^[0-9]+$/, // regexp precio
+    /^[0-9]+$/, // regexp stock
+    /^.+$/  // regexp foto
+]
+
+const camposValidos = [ false, false, false, false ]
+const algunCampoNoValido = () => {
+    let valido = 
+        camposValidos[0] &&
+        camposValidos[1] &&
+        camposValidos[2] &&
+        camposValidos[3]
+
+    return !valido
+}
 
 
 const setCustomValidity = function (mensaje, index) {
@@ -15,30 +31,60 @@ function validar(valor, validador, index) {
 
     if (!validador.test(valor)) {
         setCustomValidity('Este campo no es válido', index)
+        camposValidos[index] = false
+        button.disabled = true
         return null
     }
 
+    camposValidos[index] = true
+    button.disabled = algunCampoNoValido()
     setCustomValidity('', index)
     return valor
 }
 
 
-function renderProds() {
-    let html = ''
+function renderProds(productos) {
+    fetch('vistas/alta.hbs')
+    .then(r => r.text())
+    .then(plantilla => {
+        // compile the template
+        var template = Handlebars.compile(plantilla);
+        // execute the compiled template and print the output to the console
+        let html = template({ productos: productos });
 
-    for (let i = 0; i < productos.length; i++) {
-        // html += `<p>${productos[i]}</p>`
-        html += `<p>${JSON.stringify(productos[i])}</p>`
-    }
-
-    document.querySelector('.listado-productos').innerHTML = html
+        document.querySelector('.listado-productos').innerHTML = html
+    })
 }
 
+function leerProductoIngresado() {
+    return {
+        nombre: inputs[0].value,
+        precio: inputs[1].value,
+        stock: inputs[2].value,
+        marca: selects[0].value,
+        categoria: selects[1].value,
+        detalles: textarea.value,
+        foto: inputs[3].value,
+        envio: inputs[4].checked,
+    }
+}
 
-function initAlta() {
+function limpiarFormulario(){
+    //Inicializo los campos del formulario
+    inputs.forEach(input => {
+        input.type == 'checkbox' ? input.checked = false : input.value = ''
+    })
+    textarea.value = ''
+    selects.forEach(select => select.value = '')
+
+    button.disabled = true
+    for(let i=0; i<camposValidos.length; i++){
+        camposValidos[i] = false
+    }
+}
+
+async function initAlta() {
     console.warn('initAlta')
-
-    productos = []
 
     inputs = document.querySelectorAll('.section-add-form input')
     selects = document.querySelectorAll('.section-add-form select')
@@ -46,33 +92,27 @@ function initAlta() {
 
     form = document.querySelector('.section-add-form')
     button = document.querySelector('button')
-    
-    inputs.forEach(input => {
-        input.addEventListener('input', () => {
-            validar(input.value, /abc/, 0)
-        })
+
+    textarea.value = ''
+    button.disabled = true
+
+    productosModel.inicializar(await productosController.obtenerProductos())
+    renderProds(productosModel.obtener())
+
+    inputs.forEach((input, index) => {
+        if(input.type != 'checkbox'){
+            input.addEventListener('input', () => {
+                validar(input.value, regExpValidar[index], index)
+            })
+        }
     })
     
-    form.addEventListener('submit', e => {
+    form.addEventListener('submit', async e => {
         e.preventDefault()
     
-        let producto = {
-            nombre: inputs[0].value,
-            precio: inputs[1].value,
-            stock: inputs[2].value,
-            marca: selects[0].value,
-            categoria: selects[1].value,
-            detalles: textarea.value,
-            foto: inputs[3].value,
-            envio: inputs[4].checked,
-        }
-    
-        // console.log(producto)
-        productos.push(producto)
-        console.log(productos)
-        renderProds()
-    
-        inputs.forEach(input => input.value = '')
-    
+        let producto = leerProductoIngresado()
+        limpiarFormulario()
+
+        await productosController.guardarProducto(producto)
     })
 }
--- js/menu/inicio.js
+++ js/menu/inicio.js
@@ -1,4 +1,38 @@
-var card1 = new Card('Los 3 Puentes', 'Natación. Trelew. Tres distancias distintas, sobre el Río Chubut.', 'img/01-tw_3-puentes.jpg')
+function renderCards(productos) {
+    fetch('vistas/inicio.hbs')
+    .then(r => r.text())
+    .then(plantilla => {
+        // compile the template
+        var template = Handlebars.compile(plantilla);
+        // execute the compiled template and print the output to the console
+        let html = template({ productos: productos });
+
+        document.querySelector('.cards-container').innerHTML = html
+    })
+}
+
+function agregarAlCarrito(id){
+//    console.log('agregarAlCarrito:', id)
+    let producto = productosModel.obtener(id)
+//    console.log(producto)
+
+    carritoController.agregarAlCarrito(producto)
+}
+
+async function initInicio() {
+    console.warn('initInicio')
+
+    productosModel.inicializar(await productosController.obtenerProductos())
+    let productos = productosModel.obtener()
+
+    renderCards(productos)
+
+    let lg = productos.length
+
+    document.querySelector('.section-cards__header p').innerHTML = lg ?`Se encontraron ${lg} productos` : ''
+}
+
+/* var card1 = new Card('Los 3 Puentes', 'Natación. Trelew. Tres distancias distintas, sobre el Río Chubut.', 'img/01-tw_3-puentes.jpg')
 var card2 = new Card('Natatlón en Puerto Madryn.', 'Puerto Madryn. Primera Fecha. Distancias Sprint, Promocional, y Postas.', 'img/02-pm_natatlon.jpg')
 
 var cards = [
@@ -8,14 +42,4 @@ var cards = [
     new Card('Natación en Villa La Angostura. Carrera.', 'Playa Lago Correntoso, Villa La Angostura, Neuquén. Distancias competitiva y promocional.', 'img/04-vla_natacion.jpg'),
     new Card('Natación en Puerto Madryn. Vuelta al Folias', 'Puerto Madryn, Playa Paraná.', 'img/05-pm_folias.jpg'),
 ]
-
-
-function initInicio() {
-    console.warn('initInicio')
-
-    var elemCardsContainer = document.getElementsByClassName('cards-container')[0]
-
-    for (var card of cards) {
-        card.appendTo(elemCardsContainer)
-    }
-}
+ */
